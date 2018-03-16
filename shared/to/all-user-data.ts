907a4f35f9038b48e6c7a9517e9fc98cf81a12cd
igorr <nimbik@gmail.com>
2018-03-17 10:53:05 +1300

    Added ts models

---
+++ shared/to/all-user-data.ts
@@ -0,0 +1,9 @@
+import { Thread } from "../model/thread";
+import { Message } from "../model/message";
+import { Participant } from "../model/participant";
+
+export interface AllUserData{
+    participants:Participant[];
+    threads: Thread[];
+    messages: Message[]
+}
